1e40da1d524a7be28ff7488ca01cd5c39805c7ba
Zurab Elchibegashvili <elchibegashvili.zurab@kiu.edu.ge>
2021-10-08 11:27:11 +0200

    Changes by Online Editor

--- src/fop/w2rabbits/RabbitPopulation.java
+++ src/fop/w2rabbits/RabbitPopulation.java
@@ -3,6 +3,15 @@ package fop.w2rabbits;
 public class RabbitPopulation extends MiniJava {
 
     public static void main(String[] args){
-	// TODO
+	 int n=1, g1=1, g2=0, g3=0, k=0;
+        int month = readInt("Please insert a number:");
+        while(n<=month) {
+            k=g1+g2+g3;
+            g3=g2;
+            g2=g1;
+            g1=k;
+            n++;
+        }
+        write(k);
     }
 }
